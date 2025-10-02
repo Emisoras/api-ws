6240c81ac32bb7812e6ad23af50e73b93b9c680b
Emisoras <emisorasdelmundo2025@gmail.com>
2025-10-02 17:13:22 -0500

    Prueba Nueva6

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,10 @@ module.exports = {
     name   : "whatsapp-bot-api", // Un nombre para tu aplicación
     script : "./app.js",         // El script principal de tu aplicación 
      watch  : false,               // Reiniciar la aplicación si hay cambios en los archivos
-    // max_memory_restart: '1000M', // Reiniciar si la memoria excede 1000MB 
+     max_memory_restart: '1000M', // Reiniciar si la memoria excede 1000MB 
      exec_mode: "fork",        // Modo de ejecución en clúster 
      instances: 1,                // Número de instancias 
-     // cron_restart: "59 23 * * *", // Reiniciar todos los días a las 23:59 
+     cron_restart: "59 23 * * *", // Reiniciar todos los días a las 23:59 
      env: { 
        "PORT": 3000, 
        "HOST": "0.0.0.0" 
