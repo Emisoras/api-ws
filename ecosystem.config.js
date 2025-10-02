fdcaf40dfd2facaae83400e5dca970db97b3b468
Emisoras <emisorasdelmundo2025@gmail.com>
2025-10-02 16:47:52 -0500

    Prueba Node4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps : [{
     name   : "whatsapp-bot-api", // Un nombre para tu aplicación
-    script : "./app.js",         // El script principal de tu aplicación
-    watch  : true,               // Reiniciar la aplicación si hay cambios en los archivos
+    script : "./app.js",         // El script principal de tu aplicación 
+     watch  : false,               // Reiniciar la aplicación si hay cambios en los archivos
     // max_memory_restart: '1000M', // Reiniciar si la memoria excede 1000MB 
      exec_mode: "fork",        // Modo de ejecución en clúster 
      instances: 1,                // Número de instancias 
